9b8a25f100fbfef64ef05149468c218f5bc63f52
Philani Ncube <ncbphi001@gmail.com>
2021-12-28 20:03:16 +0200

    product grid styling

--- components/Category/ProductsGrid.jsx
+++ components/Category/ProductsGrid.jsx
@@ -8,7 +8,7 @@ const CategoryProducts = ({ products }) => {
   const { addItemToCart } = useCart();
 
   return (
-    <div className="max-w-7xl mx-auto px-4 md:px-6 lg:px-0 py-16 flex justify-center">
+    <div className="container mx-auto px-4 md:px-6 lg:px-0 py-8 flex justify-center">
       <div className="fle flex-col justify-center items-center">
         <div className="flex justify-start items-start">
           <p className="text-3xl lg:text-4xl font-semibold leading-9 text-gray-800">
